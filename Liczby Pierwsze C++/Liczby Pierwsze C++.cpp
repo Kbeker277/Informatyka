53e4eaf7422d2f12d49d3b4fd9b5f7bdb09c66f4
Kbeker277 <567iron567@wp.pl>
2022-03-04 14:09:36 +0100

    Liczby pierwsze program

---
+++ Liczby Pierwsze C++/Liczby Pierwsze C++.cpp
@@ -0,0 +1,28 @@
+﻿// Liczby Pierwsze C++.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+
+    int l;
+    l = 0;
+
+    int p = 2;
+
+    int x;
+    cout << "podaj liczbę";
+    cin >> x;
+
+    while(l < x) {
+        l = l + 1;
+        x / l = 0;
+
+    }
+    
+
+
+
+
