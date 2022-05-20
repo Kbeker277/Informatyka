6680472758e39b3d3fbb9dd595279f8050b4b680
Kbeker277 <567iron567@wp.pl>
2022-05-20 13:14:12 +0200

    Wyszukiwanie binarne

---
+++ Wyszukiwanie Binarne/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int sukanie(int tak , int t[]) {
+    
+    int a = 0;
+    int x = 15;
+    int ls = (a + x)/2;
+    while (a <= x) 
+    {
+        if (t[ls] == tak) 
+        {
+            return ls;
+        }
+        if (t[ls] > tak) 
+        {
+            x = ls - 1;
+        }
+        else 
+        {
+            a = ls + 1;
+        }
+        ls = (a + x) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    
+    int tak;
+    int a = 0;
+    int x = 7;
+    int ls = (a + x) / 2;
+    int t[15] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
+    
+    cout << "jaka liczba :  \n";
+    cin >> tak;
+    cout << sukanie(tak,t);
+}
+
+
